56303a6724be013c69d4a33d3739566c20100f24
Sushomeister <91095463+Sushomeister@users.noreply.github.com>
2022-03-30 17:27:28 +0300

    Добавьте файлы проекта.

---
+++ LAB-8.1.2/LAB-8.1.2.cpp
@@ -0,0 +1,40 @@
+﻿// LAB-8.1.1.cpp
+// Сушинський Ігор
+// Лабораторна робота №8.1
+// Пошук та заміна символів у літерному рядку.
+// Варіант 20
+// 1 завдання
+// Рекурсивний спосіб
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int Count(char* str, int i)
+{
+	if (strlen(str) < 3)
+		return 0;
+
+	if (str[i + 1] != 0)
+		if (str[i - 1] == ',' && str[i + 1] == '-')
+			return 1 + Count(str, i + 1);
+		else
+			return Count(str, i + 1);
+	else
+		return 0;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Ukrainian");
+
+	char str[101];
+
+	cout << "Ввести лiтерний рядок:" << endl;
+	cin.getline(str, 100);
+
+	cout << "Лiтерний рядок мiстить " << Count(str, 1) << " групи ', -'" << endl;
+
+	return 0;
+}
